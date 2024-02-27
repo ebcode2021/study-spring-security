08246e64c777de5361c9475f37edbbee3560a4aa
ebcode2021 <ebcode2021@gmail.com>
2024-02-27 19:56:14 +0900

    [update] insert data 순서 변경

--- chapter03/src/main/resources/data.sql
+++ chapter03/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT IGNORE INTO `spring`.`users` VALUES (NULL, 'eunbi', '1234', '1');
 INSERT IGNORE INTO `spring`.`authorities` VALUES (NULL, 'eunbi', 'write');
+INSERT IGNORE INTO `spring`.`users` VALUES (NULL, 'eunbi', '1234', '1');
 
