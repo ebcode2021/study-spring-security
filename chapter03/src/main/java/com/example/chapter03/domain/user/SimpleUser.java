42b8b20857f024ab080befce6731d19ae9d9266f
ebcode2021 <ebcode2021@gmail.com>
2024-02-27 19:44:41 +0900

    [update] @Override getAuthorities() return value 수정

--- chapter03/src/main/java/com/example/chapter03/domain/user/SimpleUser.java
+++ chapter03/src/main/java/com/example/chapter03/domain/user/SimpleUser.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 public class SimpleUser implements UserDetails {
 
@@ -15,8 +16,9 @@ public class SimpleUser implements UserDetails {
         this.password = password;
     }
 
+    @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(() -> "READ");
     }
 
     @Override
