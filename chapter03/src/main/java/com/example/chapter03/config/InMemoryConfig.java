fff17b5a6dc9ea20942a48f6ee0e47d71dd869aa
ebcode2021 <ebcode2021@gmail.com>
2024-02-27 19:56:32 +0900

    [update] query문 오타 수정

--- chapter03/src/main/java/com/example/chapter03/config/InMemoryConfig.java
+++ chapter03/src/main/java/com/example/chapter03/config/InMemoryConfig.java
@@ -4,7 +4,6 @@ import com.example.chapter03.domain.user.InMemoryUser;
 import com.example.chapter03.service.InMemoryUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,9 +15,8 @@ import javax.sql.DataSource;
 public class InMemoryConfig {
     @Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
-        String usersByUsernameQuery = "select username, password, enabled from users where username = ?";
+        String usersByUsernameQuery = "select username, password, enabled from spring.users where username = ?";
         String authByUsernameQuery = "select username, authority from spring.authorities where username = ?";
-        String authByPasswordQuery ="a";
 
         JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
         userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
