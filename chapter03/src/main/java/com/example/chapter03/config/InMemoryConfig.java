c8e796ebb861e2df69bb5b4375c59c263e365b7a
ebcode2021 <ebcode2021@gmail.com>
2024-02-27 19:45:17 +0900

    [update] JdbcUserDetailsManager 쿼리 재정의하여 사용

---
+++ chapter03/src/main/java/com/example/chapter03/config/InMemoryConfig.java
@@ -0,0 +1,33 @@
+package com.example.chapter03.config;
+
+import com.example.chapter03.domain.user.InMemoryUser;
+import com.example.chapter03.service.InMemoryUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class InMemoryConfig {
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        String usersByUsernameQuery = "select username, password, enabled from users where username = ?";
+        String authByUsernameQuery = "select username, authority from spring.authorities where username = ?";
+        String authByPasswordQuery ="a";
+
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authByUsernameQuery);
+        return userDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
