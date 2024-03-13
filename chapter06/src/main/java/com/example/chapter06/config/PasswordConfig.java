67b0aede9a0af1c69006761511f6594aa78806ac
ebcode2021 <ebcode2021@gmail.com>
2024-03-13 20:32:41 +0900

    [add] PasswordConfig

---
+++ chapter06/src/main/java/com/example/chapter06/config/PasswordConfig.java
@@ -0,0 +1,19 @@
+package com.example.chapter06.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SCryptPasswordEncoder sCryptPasswordEncoder() {
+        return new SCryptPasswordEncoder(16384, 8, 1, 32, 64);
+    }
+}
