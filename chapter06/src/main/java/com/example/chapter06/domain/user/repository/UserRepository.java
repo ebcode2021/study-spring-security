d06ffe340ab04fdb0b1d8811fee09d86616d0b6a
ebcode2021 <ebcode2021@gmail.com>
2024-03-13 20:32:54 +0900

    [add] UserRepository

---
+++ chapter06/src/main/java/com/example/chapter06/domain/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.chapter06.domain.user.repository;
+
+import com.example.chapter06.domain.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findUserByUsername(String u);
+}
