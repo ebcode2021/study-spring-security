7366d0d3c1c72291f8046a097480f7ce12016a3d
ebcode2021 <ebcode2021@gmail.com>
2024-03-14 15:47:55 +0900

    [add] ProductService

---
+++ chapter06/src/main/java/com/example/chapter06/domain/product/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.example.chapter06.domain.product.service;
+
+import com.example.chapter06.domain.product.model.Product;
+import com.example.chapter06.domain.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+}
