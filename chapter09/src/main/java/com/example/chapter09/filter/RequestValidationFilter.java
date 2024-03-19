9974201c8e348f93e92a3ce424d63a5d995355f1
ebcode2021 <ebcode2021@gmail.com>
2024-03-19 15:24:18 +0900

    [add] 필수적으로 Request-Id Header가 있는 요청만 받도록. 실패하면 400

---
+++ chapter09/src/main/java/com/example/chapter09/filter/RequestValidationFilter.java
@@ -0,0 +1,26 @@
+package com.example.chapter09.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class RequestValidationFilter implements Filter {
+
+    // 필터 논리 작성
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        String requestId = httpRequest.getHeader("Request-Id");
+
+        if (requestId == null || requestId.isBlank()) {
+            httpResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return ;
+        }
+
+        chain.doFilter(request, response);
+    }
+}
