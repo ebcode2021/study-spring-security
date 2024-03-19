f510fad8c5463be7076cce0185ad8226c1ca71fc
ebcode2021 <ebcode2021@gmail.com>
2024-03-19 15:23:36 +0900

    [add] 메서드 체이닝 대신 람다식 사용하여 SecurityFilterChain 세팅

---
+++ chapter09/src/main/java/com/example/chapter09/config/FilterConfig.java
@@ -0,0 +1,23 @@
+package com.example.chapter09.config;
+
+import com.example.chapter09.filter.RequestValidationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class FilterConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.addFilterBefore( // 필터 체인에서 인증 필터 앞에 맞춤형 필터의 인스턴스 추가
+                new RequestValidationFilter(),
+                BasicAuthenticationFilter.class
+        ).authorizeHttpRequests((authz) ->
+                authz.anyRequest().permitAll());
+        return http.build();
+    }
+}
