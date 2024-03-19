b939c683a440b56efa2067b2c78bbbccd4c0fd83
ebcode2021 <ebcode2021@gmail.com>
2024-03-19 15:41:18 +0900

    [add] request header에 대한 로깅 필터

---
+++ chapter09/src/main/java/com/example/chapter09/config/AuthenticationLogginFilter.java
@@ -0,0 +1,25 @@
+package com.example.chapter09.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class AuthenticationLogginFilter implements Filter {
+
+    private final Logger logger = Logger.getLogger(
+            AuthenticationLogginFilter.class.getName()
+    );
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+        String requestId = httpRequest.getHeader("Request-Id");
+
+        logger.info("인증 성공! request-id : " + requestId);
+
+        chain.doFilter(request, response); // 요청을 필터 체인의 다음 필터에 전달
+    }
+}
