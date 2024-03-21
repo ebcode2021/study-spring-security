df042e160f3b21f7671e76d12dc6716c9c503e83
ebcode2021 <ebcode2021@gmail.com>
2024-03-21 20:01:04 +0900

    [add] 도메인 별 Controller 메서드 작성

---
+++ chapter10/src/main/java/com/example/chapter10/controller/ProductController.java
@@ -0,0 +1,21 @@
+package com.example.chapter10.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+    private Logger logger = LoggerFactory.getLogger(ProductController.class);
+
+    @PostMapping("/add")
+    public String add(@RequestParam("name") String name) {
+        logger.info("Adding product : " + name);
+
+        return "main.html";
+    }
+}
