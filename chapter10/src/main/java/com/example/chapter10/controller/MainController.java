6e511ce2ee75896cc98b79c250dc406181732fbf
ebcode2021 <ebcode2021@gmail.com>
2024-03-28 12:44:53 +0900

    [update] @CrossOrigin() 을 사용하여 CORS 허용

--- chapter10/src/main/java/com/example/chapter10/controller/MainController.java
+++ chapter10/src/main/java/com/example/chapter10/controller/MainController.java
@@ -3,6 +3,7 @@ package com.example.chapter10.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +20,7 @@ public class MainController {
 
     @PostMapping("/test")
     @ResponseBody
+    @CrossOrigin("http://localhost:8080") //localhost 출처에 대한 교차 출처 요청을 허용
     public String test() {
         logger.info("Test method called");
         return "HELLO!";
