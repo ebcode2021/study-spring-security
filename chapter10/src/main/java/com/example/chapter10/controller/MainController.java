523ddec3c550e08b65e6599a4538ab64d608d76e
ebcode2021 <ebcode2021@gmail.com>
2024-03-28 12:34:07 +0900

    [update] /test 엔드포인트 매핑

--- chapter10/src/main/java/com/example/chapter10/controller/MainController.java
+++ chapter10/src/main/java/com/example/chapter10/controller/MainController.java
@@ -1,13 +1,26 @@
 package com.example.chapter10.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
+    private final Logger logger = LoggerFactory.getLogger(MainController.class);
+
     @GetMapping("/main")
     public String main() {
         return "main.html";
+    }
+
+    @PostMapping("/test")
+    @ResponseBody
+    public String test() {
+        logger.info("Test method called");
+        return "HELLO!";
     }
 }
