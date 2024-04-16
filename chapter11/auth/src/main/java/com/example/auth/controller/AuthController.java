f4267ba7f2d28755842ba4f6592b16b13ca4da7c
ebcode2021 <ebcode2021@gmail.com>
2024-04-16 17:25:33 +0900

    [feat] auth 엔드포인트

---
+++ chapter11/auth/src/main/java/com/example/auth/controller/AuthController.java
@@ -0,0 +1,37 @@
+package com.example.auth.controller;
+
+import com.example.auth.model.Otp;
+import com.example.auth.model.User;
+import com.example.auth.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/user/add")
+    public void addUser(@RequestBody User user) {
+        userService.addUser(user);
+    }
+
+    @PostMapping("/user/auth")
+    public void auth(@RequestBody User user) {
+        userService.auth(user);
+    }
+
+    @PostMapping("/otp/check")
+    public void check(@RequestBody Otp otp, HttpServletResponse response) {
+        // OTP가 유효하면 200, 아니면 403
+        if (userService.check(otp)) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        }
+    }
+}
